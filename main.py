b9149fd3d92642c8b7713787c589cb9c42fef5ef
Ahmad Habibie Nazri <nazri@Ahmads-MacBook-Pro.local>
2023-10-24 12:53:34 +0700

    adding path buku

--- main.py
+++ main.py
@@ -51,4 +51,17 @@ async def read_user(username: str, db: db_dependency):
     user = db.query(models.User).filter(models.User.username == username).first()
     if user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User Not Found')
-    return user
+    return user 
+
+@app.post("/buku/", status_code=status.HTTP_201_CREATED)
+async def create_buku(buku: BukuBase, db: db_dependency):
+    db_buku = models.Buku(**buku.dict())
+    db.add(db_buku)
+    db.commit()
+
+@app.get("/buku/{judul}", status_code=status.HTTP_200_OK)
+async def read_user(judul: str, db: db_dependency):
+    buku = db.query(models.Buku).filter(models.Buku.judul == judul).first()
+    if buku is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Buku Not Found')
+    return buku 
